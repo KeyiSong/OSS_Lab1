0da33bf57580883dd429b37119427a34cc3af43f
wyh202102 <126311938+wyh202102@users.noreply.github.com>
2023-11-08 17:14:23 +0800

    Update addLine.py

--- addLine.py
+++ addLine.py
@@ -1,6 +1,6 @@
 # 给测试集添加标签
 import csv
-
+#我看不懂，但我大受震撼
 with open(r'..\test\sentiment-analysis\test.csv') as csvfile:
     rows = csv.reader(csvfile)
     with open(r'..\test\sentiment-analysis\test1.csv', 'a+', newline='') as f:
